e8232ade5e0229b1b19954db7d944ee50cf61060
RoanOak127 <54964349+RoanOak127@users.noreply.github.com>
2020-08-20 18:30:56 -0500

    Finished Tutorial!
    Everything works, there's sounds, there's movements, the level increases like it should!

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -5,6 +5,8 @@ public class Wall : MonoBehaviour
 {
     public Sprite dmgSprite;
     public int hp = 4;
+    public AudioClip chopWall1;
+    public AudioClip chopWall2;
 
     private SpriteRenderer spriteRenderer;
 
@@ -16,6 +18,7 @@ public class Wall : MonoBehaviour
 
     public void DamageWall (int loss)
     {
+        SoundManager.instance.RandomizeSfx(chopWall1, chopWall2);
         spriteRenderer.sprite = dmgSprite;
         hp -= loss;
         if (hp <= 0)
