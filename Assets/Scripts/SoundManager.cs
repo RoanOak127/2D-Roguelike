f99b8c5956fb8ad644c5daf74bf3c3e89e9c8192
RoanOak127 <54964349+RoanOak127@users.noreply.github.com>
2020-08-20 17:39:19 -0500

    Sound Script
    Game crashed again, couldn't save the manager but the sound scripts are here

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+    public static SoundManager instance = null;
+
+    public float lowPitchRange = 0.95f;
+    public float highPitchRange = 1.05f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySingle (AudioClip clip)
+    {
+        efxSource.clip = clip;
+        efxSource.Play();
+    }
+
+    public void RandomizeSfx (params AudioClip[] clips)
+    {
+        int randomIx = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+        efxSource.pitch = randomPitch;
+        efxSource.clip = clips[randomIx];
+        efxSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
